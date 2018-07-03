7ac2c874f937d1b20cccf7cec1b9401279a843ce
SanAndreasP <1294@live.de>
2018-07-03 21:17:55 +0200

    bumped revision number

--- src/main/java/de/sanandrew/mods/immersivecables/util/ICConstants.java
+++ src/main/java/de/sanandrew/mods/immersivecables/util/ICConstants.java
@@ -16,7 +16,7 @@ public final class ICConstants
 
     public static final String ID = "immersivecables";
     public static final Logger LOG = LogManager.getLogger(ID);
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.3.1";
     public static final String NAME = "Immersive Wiring";
     public static final String DEPENDENCIES = "required-after:forge@[14.23.1.2582,];required-after:immersiveengineering@[0.12-78,];" +
                                                       "after:" + COMPAT_APPLIEDENERGISTICS + ';' +
